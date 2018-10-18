0dc7013f02a163a025745eae7085586847fd32e4
Danilo da Silva Pereira <danilo.pereira@basis.com.br>
2018-10-18 16:47:21 -0300

    07 (Part. 02.Final) - Reduzindo de N ifs para nenhum com Strategy em Java  - by Alex Felipe (Alura)

--- src/br/com/danilo/artigos/alura/designpatterns/strategy/Cargo.java
+++ src/br/com/danilo/artigos/alura/designpatterns/strategy/Cargo.java
@@ -1,6 +1,43 @@
 package br.com.danilo.artigos.alura.designpatterns.strategy;
 
-public enum Cargo {
+public enum Cargo implements Comissao {
 
-    ATENDENTE, VENDEDOR, GERENTE, AJUDANTE, RECEPCIONISTA, DIRETOR;
+    ATENDENTE {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.1;
+        }
+    },
+    VENDEDOR {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.15 + 5;
+        }
+    },
+    GERENTE {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.20 + 10;
+        }
+    },
+    AJUDANTE {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.08 + 1;
+        }
+    },
+    RECEPCIONISTA {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.05;
+        }
+    },
+    DIRETOR {
+        @Override
+        public double calculaComissao(double valor) {
+            return valor * 0.25 + 20;
+        }
+    }
+
+
 }
---
+++ src/br/com/danilo/artigos/alura/designpatterns/strategy/Comissao.java
@@ -0,0 +1,6 @@
+package br.com.danilo.artigos.alura.designpatterns.strategy;
+
+public interface Comissao {
+
+    double calculaComissao(double valor);
+}
